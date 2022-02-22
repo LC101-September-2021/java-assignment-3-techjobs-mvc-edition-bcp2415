736f11d2b451e463d108a491f9b3621a927e53b5
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-02-22 07:15:58 -0600

    updating

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -3,7 +3,9 @@ package org.launchcode.techjobs.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -21,6 +23,11 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    // + 2 more parameters for type of search & search term
+    public String displaySearchResults(Model model, @RequestParam searchType, @RequestParam searchTerm) {
+        //do stuff
+
+    }
 
 }
